5f8d75ad65895931c298a52b8b191d2d60b14d3b
wassy310 <ryg.wsza@gmail.com>
2023-03-08 12:41:17 +0900

    Creating a state that holds a task list

--- todo-app/src/App.tsx
+++ todo-app/src/App.tsx
@@ -1,14 +1,25 @@
+import { useState } from 'react';
+
+type Todo = {
+  value: string;
+};
+
 export const App = () => {
+  const [text, setText] = useState('');
+  const [todos, setTodos] = useState<Todo[]>([]);
   return (
     <div>
-      <form onSubmit={(e) => e.preventDefault()}>
-        <input type="text" value="" onChange={(e) => e.preventDefault()} />
+      <form onSubmit = {(e) => e.preventDefault()}>
         <input
-          type="submit"
-          value="追加"
-          onSubmit={(e) => e.preventDefault()}
+          type = "text"
+          value = {text}
+          onChange = {(e) => setText(e.target.value)}
+        />
+        <input
+          type = "submit"
         />
       </form>
+      <p>{text}</p>
     </div>
   );
 };
